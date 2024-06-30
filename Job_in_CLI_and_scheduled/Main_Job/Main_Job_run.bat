41083d9897cc83e184951a2e74e5616682f4900a
Moundji25 <moundjibel@hotmail.com>
2024-06-30 14:51:37 +0200

    added the cli job + the zip version

---
+++ Job_in_CLI_and_scheduled/Main_Job/Main_Job_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/log4j-1.2-api-2.12.1.jar;../lib/commons-collections-3.2.2.jar;../lib/mysql-connector-java-8.0.18.jar;../lib/trove.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/jxl.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;../lib/jboss-serialization.jar;../lib/talendcsv.jar;../lib/crypto-utils.jar;../lib/talend_file_enhanced_20070724.jar;main_job_0_1.jar;job01_customer_dim_0_1.jar;job04_geography_dim_0_1.jar;job05_order_fact_0_1.jar;job03_organization_dim_0_1.jar;job02_product_dim_0_1.jar; etude_de_cas_talend.main_job_0_1.Main_Job  --context=Default %*
